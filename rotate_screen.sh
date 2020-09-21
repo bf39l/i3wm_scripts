eb697734915bff271a5772ce6a6e3a2fe814b2a5
Bo Liu <boliu1234@gmail.com>
2020-09-21 16:20:33 +1200

    - updated rotate_screen script and battery script - battery script has a known bug: battery number changed when multiple battery sources connected (eg: bluetooth mouse)

--- rotate_screen.sh
+++ rotate_screen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # invert_screen
-orientation="$(xrandr -q | grep "eDP-1 connected" | awk '{print $5}')"
+#orientation="$(xrandr -q | grep "eDP-1 connected" | awk '{print $5}')"
+orientation=$(xrandr -q | grep "eDP-1 connected" | cut -d '(' -f 1 | grep -wo inverted)
+echo $orientation
 display="eDP-1"
 if [ "$orientation" = "inverted" ]; then
    xrandr --output $display --rotate normal
